81168151c0f76ecd4ef4db75c80cb4583ae6d183
Adeyinka Adepoju <a.adepoju@wustl.edu>
2025-11-10 00:24:31 -0600

    Added the argument i to handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  
+
   function handleClick() {
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    nextSquares[i] = "X";
     setSquares(nextSquares);
   }
 
